dad2ea4e5497814efd8a2b4bc3a912b8173c798c
Soumya Rajeesh <soumyatk@uw.edu>
2023-11-21 01:02:10 -0800

    jQueryToDoList

--- carProto.js
+++ carProto.js
@@ -13,3 +13,52 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+// Define the Car class
+class Car {
+    constructor(model) {
+      this.model = model;
+      this.currentSpeed = 0;
+    }
+  
+    accelerate() {
+      this.currentSpeed += 1;
+    }
+  
+    brake() {
+      this.currentSpeed -= 1;
+    }
+  
+    toString() {
+      return `Car Model: ${this.model}, Current Speed: ${this.currentSpeed}`;
+    }
+  }
+  
+  // Define the ElectricCar class that inherits from Car
+  class ElectricCar extends Car {
+    constructor(model) {
+      super(model);
+      this.motor = "electric";
+    }
+  
+    accelerate() {
+      super.accelerate(); // Call the accelerate method of the parent class (Car) twice
+      super.accelerate();
+    }
+  
+    toString() {
+      return `Electric Car Model: ${this.model}, Motor: ${this.motor}, Current Speed: ${this.currentSpeed}`;
+    }
+  }
+  
+  // Create an instance of ElectricCar
+  const electricCar = new ElectricCar("Tesla Model 3");
+  
+  // Accelerate twice (faster acceleration)
+  electricCar.accelerate();
+  electricCar.accelerate();
+  
+  // Brake once
+  electricCar.brake();
+  
+  // Console log the instance using the toString method
+  console.log(electricCar.toString());
--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -13,3 +13,20 @@
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
 // and replaced with text that says "Added to cart"
+$(document).ready(function () {
+    const imgColor = $("img").data("color");
+    console.log(imgColor);
+  
+    $("li:eq(2)").addClass("highlight");
+  
+    $("<a>")
+      .text("Buy Now!")
+      .attr("id", "cta")
+      .insertAfter("p:last");
+  
+    $("p:last").remove();
+  
+    $("#cta").click(function () {
+      $(this).replaceWith("Added to cart");
+    });
+  });
